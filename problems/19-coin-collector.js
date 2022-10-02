9235feb74a59d65dde67679eb39b52624cf20267
flyingpigs4u <elicholai@hotmail.com>
2022-10-02 11:30:04 -0700

    19

--- problems/19-coin-collector.js
+++ problems/19-coin-collector.js
@@ -25,7 +25,28 @@ Example 3:
 
 ***********************************************************************/
 
-// your code here
+coinCollector = numCoins => {
+  let results = []                                // create results array 
+    return function coins (coinAmount) {
+      for (let i = 0; i < numCoins; i++) {      // function loops through numCoins amount of times
+        results.push(coinAmount)                    // add to array 
+        return results
+      }
+    }
+}
+
+
+// let oneCoin = coinCollector(1); // returns a function
+// console.log(oneCoin(10)); // prints [10]
+
+let twoCoins = coinCollector(2); // returns a function
+twoCoins(25); // returns a function
+console.log(twoCoins(10)); // prints [25, 10]
+
+// let threeCoins = coinCollector(3); // returns a function
+// threeCoins(25); // returns a function
+// threeCoins(5); // returns a function
+// console.log(threeCoins(10)); // prints [ 25, 5, 10 ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
